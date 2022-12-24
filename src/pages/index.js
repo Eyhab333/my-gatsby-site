7cd364ba7cf13d31cd7bab917bf6adb37f129e94
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2022-12-24 09:54:49 +0300

    useStaticQuery

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 
 import * as React from 'react'
 import Layout from '../components/layout'
-
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="a tree"
+        src="../images/pexels-pixabay-36717.jpg"
+      />
     </Layout>
   )
 }
